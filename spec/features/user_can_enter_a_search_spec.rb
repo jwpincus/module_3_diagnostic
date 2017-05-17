87fecc73fd7fc9cdee10efed981709a6fb301ad9
Jack Pincus <jwpincus@gmail.com>
2017-05-17 11:07:11 -0600

    add VCR to feature tests

--- spec/features/user_can_enter_a_search_spec.rb
+++ spec/features/user_can_enter_a_search_spec.rb
@@ -10,16 +10,18 @@ require 'rails_helper'
    end
    context 'I can search by a zip code' do
      it 'and have stations shown to me' do
-       visit '/'
-       fill_in 'zip_code', with: "80203"
-       click_on 'Locate'
-       expect(page).to have_current_path("/search?utf8=%E2%9C%93&zip_code=80203&commit=Locate")
-       expect(page).to have_selector('div.station', count: 10)
-       expect(page).to have_content('Fuel Types: ELEC' || 'Fuel Types: LPG')
-       expect(page).to_not have_content('CNG')
-       expect(page).to have_content('Address: 800 Acoma St')
-       expect(page).to have_content('Access times: MO: 12:00am-12:00am; TU: 12:00am-12:00am; WE: 12:00am-12:00am; TH: 12:00am-12:00am; FR: 12:00am-12:00am; SA: 12:00am-12:00am; SU: 12:00am-12:00am')
-       expect(page).to have_content('Distance: 0.88883')
+       VCR.use_cassette 'features/stations' do
+         visit '/'
+         fill_in 'zip_code', with: "80203"
+         click_on 'Locate'
+         expect(page).to have_current_path("/search?utf8=%E2%9C%93&zip_code=80203&commit=Locate")
+         expect(page).to have_selector('div.station', count: 10)
+         expect(page).to have_content('Fuel Types: ELEC' || 'Fuel Types: LPG')
+         expect(page).to_not have_content('CNG')
+         expect(page).to have_content('Address: 800 Acoma St')
+         expect(page).to have_content('Access times: MO: 12:00am-12:00am; TU: 12:00am-12:00am; WE: 12:00am-12:00am; TH: 12:00am-12:00am; FR: 12:00am-12:00am; SA: 12:00am-12:00am; SU: 12:00am-12:00am')
+         expect(page).to have_content('Distance: 0.88883')
+       end
      end
    end
  end
--- spec/services/stations_service_spec.rb
+++ spec/services/stations_service_spec.rb
@@ -5,8 +5,7 @@ describe "Service can make an api call" do
     VCR.use_cassette 'service/stations' do
       response = StationService.new('80218')
       response = response.stations
-      response.first.should include("station_name" => 'Walgreens - Denver, CO #1769', "street_address" => "2000 E Colfax")
-
+      response.first.should include("station_name" => 'Walgreens - Denver, CO #1769', "street_address" => "2000 E Colfax", "fuel_type_code" => 'ELEC', "distance" => 0.70964)
     end
   end
 end
