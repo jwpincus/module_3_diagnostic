dce0690b61183c867012ffa943543c0bcb2480bf
Jack Pincus <jwpincus@gmail.com>
2017-05-17 10:48:48 -0600

    logic pushed out of controller to PORO

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,3 @@ class SearchController < ApplicationController
 
   end
 end
-
-JSON.parse (Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_key']}&location=#{params[:zip_code]}&radius=6.0&fuel_type=ELEC,LPG").body)
-@stations = @stations['fuel_stations'][0..9]
--- app/services/station_service.rb
+++ app/services/station_service.rb
@@ -3,6 +3,10 @@ class StationService
     @zip_code = zip_code
   end
 
+  def stations
+    JSON.parse(Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_key']}&location=#{zip_code}&radius=6.0&fuel_type=ELEC,LPG").body)['fuel_stations'][0..9]
+  end
+
   private
   attr_reader :zip_code
 end
